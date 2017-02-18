f056ee4c007d35d3782d0be70f890789915a3e87
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2017-02-18 02:24:25 -0200

    Initial sketch

---
+++ src/Serilog.Sinks.MySQL/LoggerConfigurationMySQLExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Configuration;
+using Serilog.Configuration;
+using Serilog.Debugging;
+using Serilog.Events;
+using Serilog.Sinks.MySQL;
+
+namespace Serilog
+{
+	public static class LoggerConfigurationMySQLExtensions
+	{
+		public static LoggerConfiguration MySQL(this LoggerSinkConfiguration loggerConfiguration,
+			string nameOrConnectionString, string tableName = MySQLSink.DefaultTableName,
+			LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+			int batchPostingLimit = MySQLSink.DefaultBatchPostingLimit, TimeSpan? period = null,
+			IFormatProvider formatProvider = null, bool storeTimestampInUtc = false, bool autoCreateSqlTable = false)
+		{
+			if (loggerConfiguration == null)
+				throw new ArgumentNullException(nameof(loggerConfiguration));
+			if (nameOrConnectionString == null)
+				throw new ArgumentNullException(nameof(nameOrConnectionString));
+			if (tableName == null)
+				throw new ArgumentNullException(nameof(tableName));
+
+			string connectionString = GetConnectionString(nameOrConnectionString);
+			TimeSpan defaultedPeriod = period ?? MySQLSink.DefaultPeriod;
+
+			return
+					loggerConfiguration.Sink(
+						new MySQLSink(connectionString, tableName, batchPostingLimit, defaultedPeriod, formatProvider, storeTimestampInUtc,
+							autoCreateSqlTable), restrictedToMinimumLevel);
+		}
+
+		private static string GetConnectionString(string nameOrConnectionString)
+		{
+			// If there is an `=`, we assume this is a raw connection string not a named value
+			// If there are no `=`, attempt to pull the named value from config
+			if (nameOrConnectionString.IndexOf('=') < 0)
+			{
+				ConnectionStringSettings cs = ConfigurationManager.ConnectionStrings[nameOrConnectionString];
+				if (cs != null)
+					return cs.ConnectionString;
+
+				SelfLog.WriteLine(
+					"MySQL sink configured value {0} is not found in ConnectionStrings settings and does not appear to be a raw connection string.",
+					nameOrConnectionString);
+			}
+
+			return nameOrConnectionString;
+		}
+	}
+}
---
+++ src/Serilog.Sinks.MySQL/Sinks/MySQL/MySQLSink.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.PeriodicBatching;
+
+namespace Serilog.Sinks.MySQL
+{
+	public class MySQLSink : PeriodicBatchingSink
+	{
+		public const string DefaultTableName = "Logs";
+		public const int DefaultBatchPostingLimit = 100;
+		public static readonly TimeSpan DefaultPeriod = TimeSpan.FromSeconds(5);
+
+		private readonly string connectionString;
+		private readonly string tableName;
+		private readonly IFormatProvider formatProvider;
+		private readonly bool storeTimestampInUtc;
+
+		public MySQLSink(string connectionString, string tableName, int batchSizeLimit, TimeSpan period, IFormatProvider formatProvider,
+			bool storeTimestampInUtc, bool autoCreateSqlTable)
+			: base(batchSizeLimit, period)
+		{
+			this.connectionString = connectionString;
+			this.tableName = tableName;
+			this.formatProvider = formatProvider;
+			this.storeTimestampInUtc = storeTimestampInUtc;
+
+			if (autoCreateSqlTable)
+				CreateTable();
+		}
+
+		private void CreateTable()
+		{
+			string sql = $@"CREATE TABLE IF NOT EXISTS {tableName} (
+  Id         INTEGER NOT NULL AUTO_INCREMENT,
+  Timestamp  DATETIME,
+  Level      VARCHAR(20),
+  Exception  TEXT,
+  Message    TEXT,
+  Properties JSON,
+
+  PRIMARY KEY (Id)
+);";
+
+			using (var connection = new MySqlConnection(connectionString))
+			{
+				connection.Open();
+
+				using (var command = new MySqlCommand(sql, connection))
+				{
+					command.ExecuteNonQuery();
+				}
+			}
+		}
+
+		protected override async Task EmitBatchAsync(IEnumerable<LogEvent> events)
+		{
+			using (var connection = new MySqlConnection(connectionString))
+			{
+				await connection.OpenAsync()
+						.ConfigureAwait(false);
+
+				using (var command = new MySqlCommand())
+				{
+					var sql = new StringBuilder();
+					sql.Append($"insert into {tableName}(Timestamp, Level, Exception, Message, Properties)\nvalues ");
+
+					var i = 0;
+					foreach (LogEvent logEvent in events)
+					{
+						if (i > 0)
+							sql.Append(",\n");
+
+						// The param names are meaningless to keep the string smaller
+						sql.Append($"(@a{i}, @b{i}, @c{i}, @d{i}, @e{i})");
+
+						command.Parameters.AddWithValue($"a{i}",
+							storeTimestampInUtc ? logEvent.Timestamp.DateTime.ToUniversalTime() : logEvent.Timestamp.DateTime);
+						command.Parameters.AddWithValue($"b{i}", logEvent.Level.ToString());
+						command.Parameters.AddWithValue($"c{i}", logEvent.Exception != null ? logEvent.Exception.ToString() : "");
+						command.Parameters.AddWithValue($"d{i}", logEvent.RenderMessage(formatProvider));
+						command.Parameters.AddWithValue($"e{i}", ToJson(logEvent.Properties));
+
+						++i;
+					}
+
+					command.Connection = connection;
+					command.CommandText = sql.ToString();
+					await command.ExecuteNonQueryAsync()
+							.ConfigureAwait(false);
+				}
+			}
+		}
+
+		private string ToJson(IReadOnlyDictionary<string, LogEventPropertyValue> properties)
+		{
+			if (properties.Count < 1)
+				return "";
+
+			var formater = new JsonValueFormatter(typeTagName: "$type");
+
+			using (var result = new StringWriter())
+			{
+				result.Write('{');
+				var i = 0;
+				foreach (KeyValuePair<string, LogEventPropertyValue> property in properties)
+				{
+					if (i > 0)
+						result.Write(',');
+
+					result.Write('\n');
+					JsonValueFormatter.WriteQuotedJsonString(property.Key, result);
+					result.Write(": ");
+					formater.Format(property.Value, result);
+
+					++i;
+				}
+				result.Write("\n}");
+
+				return result.ToString();
+			}
+		}
+	}
+}
